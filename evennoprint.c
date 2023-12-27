28c7ca7960c23f0ad1437308e5e3cacc53a87fe1
AASIF SAIFI <140430733+aasif-saifi@users.noreply.github.com>
2023-12-27 13:31:53 +0530

    Add files via upload

---
+++ evennoprint.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int i;
+    printf("print the 1 to 20 odd numbers:");
+    for(i=0;i<=200;i+=2){
+        
+        printf("\v%d",i);
+    
+    }
+    return 0;
+    
+}
